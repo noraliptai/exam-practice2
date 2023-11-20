f4b33d54a22686ca342dc66dfbd1e0888d489b3d
Your Name <you@example.com>
2023-11-20 13:11:34 +0100

    done

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector("#root").insertAdjacentHTML("beforeend", `
+    <form>
+      <h1>Register your pet!</h1>
+
+      <input type="text" placeholder="pet name">
+
+      <textarea placeholder="symptoms..."></textarea>
+
+      <button>submit</button>
+
+      <div>
+        <input type="checkbox">
+        <p>Elolvastam és elfogadom a felhasználási feltételeket</p>
+      </div>
+    </form>
+
+    <div>?</div>
+`)
